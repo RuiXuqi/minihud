8b0891f640439092e1b617fb39962540414428b0
RuiXuqi <90179819+RuiXuqi@users.noreply.github.com>
2026-02-20 15:59:08 +0800

    Unimined

---
+++ src/main/java-templates/minihud/Tags.java
@@ -0,0 +1,11 @@
+package {{ package }};
+
+/**
+ * Tags storage class, you can change at will
+ */
+public class Tags {
+    private Tags() {}
+
+    public static final String VERSION = "{{ mod_version }}";
+
+}
